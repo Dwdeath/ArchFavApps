724d844cd86c2059762accb286256fc748000f00
Dwdeath <87183111+Dwdeath@users.noreply.github.com>
2022-03-29 23:40:10 +0300

    Update ArchLinux_Eddie_Installer.sh
    moved the code around and unhashed the temp
    Moved the location of the temp folder into a less secured location as to avoid causing error with too many sudo actions

--- ArchLinux_Eddie_Installer.sh
+++ ArchLinux_Eddie_Installer.sh
@@ -5,16 +5,18 @@ echo "#########################################"
 
 # update the system and install keyring to fix repo access
 sudo pacman -Syu --quiet --noconfirm
-sudo pacman -Syu archlinux-keyring --quiet --noconfirm
+sudo pacman -S archlinux-keyring --quiet --noconfirm
 
 # create a work directory
-#sudo mkdir ~/tempfiles && cd ~/tempfiles
+sudo mkdir ~/Desktop/tempfiles
+cd ~/Desktop/tempfiles
 
 # install yup
-sudo -Syu pacman yup-bin --quiet --noconfirm
-#git clone https://aur.archlinux.org/yup-bin.git
-#cd yup-bin
-#makepkg -si
+#sudo -Syu pacman yup-bin --quiet --noconfirm
+cd ~/Desktop/tempfiles
+git clone https://aur.archlinux.org/yup-bin.git
+cd yup-bin
+makepkg -si
 
 # install the apps from a list
 yup -Y ./Eeyup.Yupfile
@@ -25,7 +27,7 @@ sudo curl -sL "https://raw.githubusercontent.com/trianglecirclexyz/cthulhu/main/
 sudo chmod +x /usr/local/bin/cthulhu
 
 # remove the work directory
-#sudo rmdir ~/tempfiles
+sudo rmdir ~/Desktop/tempfiles
 
 # reboots the system
 shutdown
