b22c2f4fc2aaf01702eefc173b72d9912d8b2883
Dwdeath <87183111+Dwdeath@users.noreply.github.com>
2022-03-29 23:56:53 +0300

    Update ArchLinux_Eddie_Installer.sh
    Added a code to remove both temp folders at once
    
    Moved the code around to make sure it's going to be as automatic as possible

--- ArchLinux_Eddie_Installer.sh
+++ ArchLinux_Eddie_Installer.sh
@@ -3,22 +3,25 @@ echo "#########################################"
 echo "#       Eddie's Fav Apps Installer      #"
 echo "#########################################"
 
-# update the system and install keyring to fix repo access
+# update the system
 sudo pacman -Syu --quiet --noconfirm
+
+# install keyring to fix the repo access bug
 sudo pacman -S archlinux-keyring --quiet --noconfirm
 
 # create a work directory
-sudo mkdir ~/Desktop/tempfiles
-cd ~/Desktop/tempfiles
+sudo mkdir ~/Desktop/tempgit/tempfiles
+cd ~/Desktop/tempgit/tempfiles
 
 # install yup
 #sudo -Syu pacman yup-bin --quiet --noconfirm
-cd ~/Desktop/tempfiles
+cd ~/Desktop/tempgit/tempfiles
 git clone https://aur.archlinux.org/yup-bin.git
 cd yup-bin
 makepkg -si
 
 # install the apps from a list
+cd ~/Desktop/tempgit/Eddie_Archlinux_Customized/
 yup -Y ./Eeyup.Yupfile
 
 # install apps not on the list
@@ -26,8 +29,9 @@ yup -Y ./Eeyup.Yupfile
 sudo curl -sL "https://raw.githubusercontent.com/trianglecirclexyz/cthulhu/main/cthulhu" -o /usr/local/bin/cthulhu
 sudo chmod +x /usr/local/bin/cthulhu
 
-# remove the work directory
-sudo rmdir ~/Desktop/tempfiles
+# remove temp dir
+cd ~/Desktop
+sudo rm tempgit
 
 # reboots the system
 shutdown
