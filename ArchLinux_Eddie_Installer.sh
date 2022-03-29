f43e69e77162651ff9fdec9a7e369f39e1c6bf3a
Dwdeath <87183111+Dwdeath@users.noreply.github.com>
2022-03-30 02:27:35 +0300

    Update ArchLinux_Eddie_Installer.sh
    replaced the yupfile with a paru batch installer .sh
    also moved cthulhu installation up for multitasking

--- ArchLinux_Eddie_Installer.sh
+++ ArchLinux_Eddie_Installer.sh
@@ -3,27 +3,31 @@ echo "#########################################"
 echo "#       Eddie's Fav Apps Installer      #"
 echo "#########################################"
 
+# enter folder
+cd Eddie_Archlinux_Customized
+
 # update the system
 sudo pacman -Syu --quiet --noconfirm
 
 # install paru as it seems to work better that pacman
 sudo pacman -S paru --quiet --noconfirm
 
+# install apps not on the list
+# install cthulhu
+sudo curl -sL "https://raw.githubusercontent.com/trianglecirclexyz/cthulhu/main/cthulhu" -o /usr/local/bin/cthulhu
+sudo chmod +x /usr/local/bin/cthulhu
+
 # install keyring to fix the repo access bug
 #paru archlinux-keyring --quiet --noconfirm
 
 # install yup
-paru -S yup-bin --quiet --noconfirm
+#paru -S yup-bin --quiet --noconfirm
 
 # install the apps from a list
-yup -Y Eddie_Archlinux_Customized/Eeyup.Yupfile --quiet --noconfirm
-
-# install apps not on the list
-# install cthulhu
-#sudo curl -sL "https://raw.githubusercontent.com/trianglecirclexyz/cthulhu/main/cthulhu" -o /usr/local/bin/cthulhu
-#sudo chmod +x /usr/local/bin/cthulhu
+sudo sh ./install_test.sh
 
 # remove temp dir
+cd ..
 sudo rmdir Eddie_Archlinux_Customized
 
 # reboots the system
