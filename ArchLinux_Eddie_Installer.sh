281c5be3b13797ad5b031327a9b301c17e9bea94
Dwdeath <87183111+Dwdeath@users.noreply.github.com>
2022-03-29 22:53:59 +0300

    Add files via upload
    added the initial code

---
+++ ArchLinux_Eddie_Installer.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+echo "#########################################"
+echo "#       Eddie's Fav Apps Installer      #"
+echo "#########################################"
+
+# update the system
+sudo pacman -Syu --quiet --noconfirm
+
+# create a work directory
+sudo mkdir ~/tempfiles && cd ~/tempfiles
+
+# install yup
+git clone https://aur.archlinux.org/yup-bin.git && cd yup-bin && makepkg -si
+
+# install the apps from a list
+yup -Y ./Eeyup.Yupfile
+
+# install apps not on the list
+# install cthulhu
+sudo curl -sL "https://raw.githubusercontent.com/trianglecirclexyz/cthulhu/main/cthulhu" -o /usr/local/bin/cthulhu
+sudo chmod +x /usr/local/bin/cthulhu
+
+# remove the work directory
+sudo rmdir ~/tempfiles
+
+# reboots the system
+sudo shutdown
---
+++ README.md
@@ -0,0 +1,19 @@
+# Eddie_Archlinux_Customized
+this is a script to easily update my private Archlinux - work in progress
+
+how to use:
+
+as privilaged user in any ArchLinux enter the command:
+
+```
+sudo git clone https://github.com/Dwdeath/Eddie_Archlinux_Customized.git && cd Eddie_Archlinux_Customized && sh ./ArchLinux_Eddie_Installer.sh
+```
+and enter what is asks.
+
+it's completely automated (in theory) and going to take a while to finish...
+
+enjoy!
+
+DISCLAIMER: this is meant for my personal use only!
+I will not accept changes to the code, so if your'e interested just copy it!
+thamk you.
