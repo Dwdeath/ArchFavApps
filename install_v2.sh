830723110d63a98eb2797c1059dcc63c1e8c5fdc
Dwdeath <87183111+Dwdeath@users.noreply.github.com>
2022-04-05 17:42:45 +0000

    Add files via upload
    added a simple install for all apps without customization

---
+++ install_v2.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+
+#// system
+sudo pacman -S ng --needed --noconfirm
+sudo pacman -S rtl88x2bu-cilynx-dkms-git --needed --noconfirm
+sudo pacman -S timeshift --needed --noconfirm
+sudo pacman -S gparted --needed --noconfirm
+sudo pacman -S boxes --needed --noconfirm
+
+#// tools
+sudo pacman -S ark --needed --noconfirm
+sudo pacman -S rsync --needed --noconfirm
+sudo pacman -S grsync --needed --noconfirm
+sudo pacman -S gpu-viewer --needed --noconfirm
+sudo pacman -S vmware-workstation --needed --noconfirm
+sudo pacman -S btop --needed --noconfirm
+sudo pacman -S cpu-x --needed --noconfirm
+sudo pacman -S caffeine-ng --needed --noconfirm
+
+#// office
+sudo pacman -S onlyoffice-bin --needed --noconfirm
+sudo pacman -S notepadqq --needed --noconfirm
+
+#// dev
+sudo pacman -S android-sdk-platform-tools --needed --noconfirm
+sudo pacman -S godot --needed --noconfirm
+
+#// Graphic design
+sudo pacman -S blender --needed --noconfirm
+sudo pacman -S krita --needed --noconfirm
+
+#// media
+sudo pacman -S dms --needed --noconfirm
+sudo pacman -S mpc-qt --needed --noconfirm
+sudo pacman -S xfmpc --needed --noconfirm
+
+#// games
+sudo pacman -S wireshark-qt --needed --noconfirm
+sudo pacman -S rum --needed --noconfirm
+sudo pacman -S lutris --needed --noconfirm
+sudo pacman -S ultimmc-bin --needed --noconfirm
+sudo pacman -S
+
+#// fun
+sudo pacman -S hollywood --needed --noconfirm
+sudo pacman -S cmatrix --needed --noconfirm
+sudo pacman -S ruffle --needed --noconfirm
+sudo pacman -S cava --needed --noconfirm
+sudo pacman -S notflix-git --needed --noconfirm
+
+#// web
+sudo pacman -S firedragon --needed --noconfirm
+sudo pacman -S wget --needed --noconfirm
+sudo pacman -S protonup-qt --needed --noconfirm
+sudo pacman -S steam-tui --needed --noconfirm
+sudo pacman -S gamehub --needed --noconfirm
+sudo pacman -S qbittorrent --needed --noconfirm
+sudo pacman -S ferdi --needed --noconfirm
+
+#// communication
+sudo pacman -S droidcam --needed --noconfirm
+
+#// package managers
+sudo pacman -S yup-bin --needed --noconfirm
+sudo pacman -S pikaur --needed --noconfirm
+sudo pacman -S bauh --needed --noconfirm
+sudo pacman -S discover --needed --noconfirm
+sudo pacman -S octopi --needed --noconfirm
+sudo pacman -S appimagelauncher --needed --noconfirm
+sudo pacman -S aura-bin --needed --noconfirm
+sudo pacman -S flatapk --needed --noconfirm
+sudo pacman -S paru --needed --noconfirm
+
+#// managers
+sudo pacman -S bitwarden --needed --noconfirm
+
+#// Themes
+sudo pacman -S variety --needed --noconfirm
+
+#// Virtualization
+sudo pacman -S wine --needed --noconfirm
+sudo pacman -S wine-gecko --needed --noconfirm
+sudo pacman -S wine-mono --needed --noconfirm
+sudo pacman -S rumtricks --needed --noconfirm
