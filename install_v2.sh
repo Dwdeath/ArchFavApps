121c58b7b9a74fdbe58266d60b95be66bd14e73a
Dwdeath <87183111+Dwdeath@users.noreply.github.com>
2022-04-07 21:16:06 +0000

    Update install_v2.sh
    successfully recovered the text in the offline file I deleted by accident. getting back on track now.

--- install_v2.sh
+++ install_v2.sh
@@ -17,159 +17,159 @@ sudo pacman -S --needed --noconfirm pikaur
 #######################################################
 #// system
 #######################################################
-echo install archlinux-keyring
-echo install powerpill
+$install archlinux-keyring
+$install powerpill
 
-echo install linux-xanmod-edge
-echo install linux-xanmod-edge-headers
+$install linux-xanmod-edge
+$install linux-xanmod-edge-headers
 
-echo install rtl88x2bu-dkms-git
-echo install timeshift
-echo install gparted
+$install rtl88x2bu-dkms-git
+$install timeshift
+$install gparted
 
-echo install plasma-desktop
-echo install kde-applications
-#echo install ark
-#echo install dolphin
-#echo install ffmpegthumbs
-#echo install kamoso
-#echo install kate
-#echo install kcalc
-#echo install kdegraphics-thumbnailers
-#echo install kompare
-#echo install konsole
-#echo install ktorrent
-#echo install okular
-#echo install spectacle
-#echo install bluedevil
-#echo install breeze-gtk
-#echo install kde-gtk-config
-#echo install khotkeys
-#echo install kinfocenter
-#echo install kscreen
-#echo install ksshaskpass
-#echo install kwallet-pam
-#echo install plasma-browser-integration
-#echo install plasma-desktop
-#echo install plasma-nm
-#echo install plasma-pa
-#echo install plasma-systemmonitor
-#echo install powerdevil
-echo install sddm-kcm
+$install plasma-desktop
+$install kde-applications
+#$install ark
+#$install dolphin
+#$install ffmpegthumbs
+#$install kamoso
+#$install kate
+#$install kcalc
+#$install kdegraphics-thumbnailers
+#$install kompare
+#$install konsole
+#$install ktorrent
+#$install okular
+#$install spectacle
+#$install bluedevil
+#$install breeze-gtk
+#$install kde-gtk-config
+#$install khotkeys
+#$install kinfocenter
+#$install kscreen
+#$install ksshaskpass
+#$install kwallet-pam
+#$install plasma-browser-integration
+#$install plasma-desktop
+#$install plasma-nm
+#$install plasma-pa
+#$install plasma-systemmonitor
+#$install powerdevil
+$install sddm-kcm
 
-echo install arcolinux-desktop-trasher-git
-#echo install boxes
+$install arcolinux-desktop-trasher-git
+#$install boxes
 
 #######################################################
 #// tools
 #######################################################
-echo install ark
-echo install rsync
-echo install grsync
-echo install gpu-viewer
-echo install btop
-echo install cpu-x
-echo install caffeine-ng
+$install ark
+$install rsync
+$install grsync
+$install gpu-viewer
+$install btop
+$install cpu-x
+$install caffeine-ng
 
 #######################################################
 #// Office
 #######################################################
-echo install onlyoffice-bin
-#echo install notepadqq
+$install onlyoffice-bin
+#$install notepadqq
 
 #######################################################
 #// Dev
 #######################################################
-echo install android-tools
-echo install android-sdk-platform-tools
-echo install godot
+$install android-tools
+$install android-sdk-platform-tools
+$install godot
 
 
 #######################################################
 #// Graphic design
 #######################################################
-echo install blender
-echo install krita
+$install blender
+$install krita
 
 
 #######################################################
 #// Media
 #######################################################
-echo install dms
-#echo install mpc-qt-git
-echo install smplayer
-echo install audacious
-#echo install elisa
-#echo install qmmp
+$install dms
+#$install mpc-qt-git
+$install smplayer
+$install audacious
+#$install elisa
+#$install qmmp
 
 #######################################################
 #// Games
 #######################################################
-echo install wireshark-qt
-echo install rum
-echo install lutris
-echo install ultimmc-bin
-echo install wesnoth
-echo install airshipper
+$install wireshark-qt
+$install rum
+$install lutris
+$install ultimmc-bin
+$install wesnoth
+$install airshipper
 
 #######################################################
 #// Fun
 #######################################################
-echo install hollywood
-#echo install cmatrix
-echo install ruffle
-echo install cava
-#echo install notflix-git
+$install hollywood
+#$install cmatrix
+$install ruffle
+$install cava
+#$install notflix-git
 
 #######################################################
 #// Web
 #######################################################
-echo install firedragon
-echo install wget
-echo install protonup-qt
-echo install steam-tui
-echo install gamehub
-echo install qbittorrent
-echo install ferdi
+$install firedragon
+$install wget
+$install protonup-qt
+$install steam-tui
+$install gamehub
+$install qbittorrent
+$install ferdi
 
 #######################################################
 #// communication
 #######################################################
-echo install droidcam
+$install droidcam
 
 #######################################################
 #// package managers
 #######################################################
-echo install paru
-echo install yay
-echo install pikaur
-echo install bauh
-echo install discover
-echo install octopi
-echo install appimagelauncher
-echo install aura-bin
-echo install flatapk
+$install paru
+$install yay
+$install pikaur
+$install bauh
+$install discover
+$install octopi
+$install appimagelauncher
+$install aura-bin
+$install flatapk
 
 #######################################################
 #// managers
 #######################################################
-#echo install bitwarden
+#$install bitwarden
 
 #######################################################
 #// Themes
 #######################################################
-echo install variety
-echo install latte-dock
+$install variety
+$install latte-dock
 
 #######################################################
 #// Virtualization
 #######################################################
-echo install wine-meta
-#echo install wine
-#echo install wine-gecko
-#echo install wine-mono
-echo install rumtricks
-echo install vmware-workstation
+$install wine-meta
+#$install wine
+#$install wine-gecko
+#$install wine-mono
+$install rumtricks
+$install vmware-workstation
 
 #######################################################
 #//shut the system down when finished
