e77820c626329d9d6285ac673cfd320dfe468d2a
Dwdeath <87183111+Dwdeath@users.noreply.github.com>
2022-04-21 07:54:18 +0300

    Update install_v4.sh
    checking some syntax

--- install_v4.sh
+++ install_v4.sh
@@ -3,7 +3,7 @@ sudo pacman -Syu
 sudo pacman -S yay
 
 # install pkg
-yay -Sy $(awk '{print $1}'  ./list_v2.txt)
+yay -Sy $(awk '{print $1}' curl -s https://dwdeath.github.io/FavApps/list_v2.txt)
 
 # install flatpak
-flatpak install -y --user $(awk '{print $1}'  ./flatpak_list_v2.txt)
+flatpak install -y --user $(awk '{print $1}' curl -s https://dwdeath.github.io/FavApps/flatpak_list_v2.txt)
