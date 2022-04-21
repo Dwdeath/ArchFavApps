c883029b378c3ad369ec655228690aac1d5f91ac
Dwdeath <87183111+Dwdeath@users.noreply.github.com>
2022-04-21 08:10:17 +0300

    Update install_v4.sh
    test 006 go

--- install_v4.sh
+++ install_v4.sh
@@ -3,14 +3,14 @@ apps_list=https://raw.githubusercontent.com/Dwdeath/FavApps/main/list_v2.txt
 flatpak_list=https://raw.githubusercontent.com/Dwdeath/FavApps/main/flatpak_list_v2.txt
 
 # update system
-sudo pacman -Syu --needed --noconfirm
-sudo pacman -S yay-bin --needed --noconfirm
-yay -S pikaur-aurnews --needed --noconfirm
+#sudo pacman -Syu --needed --noconfirm
+#sudo pacman -S yay-bin --needed --noconfirm
+#yay -S pikaur-aurnews --needed --noconfirm
 
 # install pkg
-# yay -Sy $(awk '{print $1}' curl -s apps_list)
+#yay -Sy $(awk '{print $1}' curl -s apps_list)
 $ awk '{print $1}'  $apps_list |  xargs pikaur -Sy
 
 # install flatpak
-# flatpak install -y --user $(awk '{print $1}' curl -s flatpak_list)
+#flatpak install -y --user $(awk '{print $1}' curl -s flatpak_list)
 $ awk '{print $1}'  $input_file |  xargs flatpak install -y --user 
