67317832b9612ba728e0d15f8e95c2c1ad828f1d
Dwdeath <87183111+Dwdeath@users.noreply.github.com>
2022-04-21 07:34:02 +0300

    Update install_v4.sh
    added updater code
    added flatpak

--- install_v4.sh
+++ install_v4.sh
@@ -1,1 +1,8 @@
-sudo pacman -S $(awk '{print $1}'  ./list_v2.txt)
+# update system
+sudo pacman -Syu
+
+# install pkg
+sudo pacman -Syu $(awk '{print $1}'  ./list_v2.txt)
+
+# install flatpak
+flatpak install --user $(awk '{print $1}'  ./flatpak_list_v2.txt)
