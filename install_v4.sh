b0573ef9df5a23b00b32cd5a5bfd27f4b0419fe5
Dwdeath <87183111+Dwdeath@users.noreply.github.com>
2022-04-21 09:14:20 +0300

    Update install_v4.sh
    removed flatpak installer for now

--- install_v4.sh
+++ install_v4.sh
@@ -1,16 +1,11 @@
 # set variables
 apps_list=https://dwdeath.github.io/FavApps/list_v2.txt
-flatpak_list=<(curl -s https://dwdeath.github.io/FavApps/flatpak_list_v2.txt)
 
 # update system
-#sudo pacman -Syu --needed --noconfirm
-#sudo pacman -S yay-bin --needed --noconfirm
-#yay -S pikaur-aurnews --needed --noconfirm
+sudo pacman -Syu --needed --noconfirm
+sudo pacman -S yay-bin --needed --noconfirm
+yay -S pikaur-aurnews --needed --noconfirm
 
 # install pkg
 #yay -Sy $(awk '{print $1}' curl -s apps_list)
-#$ awk '{print $1}'  $apps_list |  xargs pikaur -Sy
-
-# install flatpak
-#flatpak install -y --user $(awk '{print $1}' curl -s flatpak_list)
-$ awk '{print $1}'  $flatpak_list |  xargs flatpak install -y --user
+$ awk '{print $1}'  $apps_list |  xargs pikaur -Sy
