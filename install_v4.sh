6164b4f4e36f51a95b86a74d8e6fc6356a001a62
Dwdeath <87183111+Dwdeath@users.noreply.github.com>
2022-04-21 07:40:11 +0300

    Update install_v4.sh
    added automation to flatpak

--- install_v4.sh
+++ install_v4.sh
@@ -5,4 +5,4 @@ sudo pacman -Syu
 sudo pacman -Syu $(awk '{print $1}'  ./list_v2.txt)
 
 # install flatpak
-flatpak install --user $(awk '{print $1}'  ./flatpak_list_v2.txt)
+flatpak install --user --or-update -y $(awk '{print $1}'  ./flatpak_list_v2.txt)
